cdb539b9d1bb4e1641c089cf821eff42639c2edd
Scott Taylor <smt@scott-taylors-macbook.local>
2008-05-18 23:07:08 -0400

    * Adding a spec for GitRevisionNumber

---
+++ spec/git_revision_number_spec.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe GitRevisionNumber do
+  describe "head" do
+    before :each do
+      @librarian = mock(GitRevisionNumbers::CommitLibrarian)
+      GitRevisionNumbers::CommitLibrarian.stub!(:new).and_return @librarian
+    end
+    
+    it 'should return the revision numbers' do
+      @librarian.stub!(:head_rev_number).and_return 1
+      @librarian.stub!(:abbreviated_sha1_head).and_return "abc12"
+      GitRevisionNumber.head.should == "1 (sha1: abc12)"
+    end
+  end
+end
