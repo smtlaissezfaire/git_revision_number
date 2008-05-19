6f16392511ebc416120c6069c7bd32a3948327cb
Scott Taylor <smt@scott-taylors-macbook.local>
2008-05-18 21:01:10 -0400

    * Adding GitRevisionNumber

--- lib/git_revision_number.rb
+++ lib/git_revision_number.rb
@@ -1,1 +1,15 @@
-# GitRevisionNumber
+require "#{RAILS_ROOT}/lib/git-rev-num/lib/git_rev_num"
+
+module GitRevisionNumber
+  class << self
+    def head
+      "#{librarian.head_rev_number} (sha1: #{librarian.abbreviated_sha1_head})"
+    end
+    
+  private
+  
+    def librarian
+      @librarian = GitRevisionNumbers::CommitLibrarian.new
+    end
+  end
+end
